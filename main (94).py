7bdc2c59ad46f85abf2239773c179c5090191fcc
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-08 12:35:30 -0400

    Add files via upload

---
+++ main (94).py
@@ -0,0 +1,82 @@
+print("Welcome to Dhruv's Currency Converter!")
+val=str(input('Enter the output currency code:'))
+if(val=='INR'):
+    USD=float(input('Enter the amount in USD:'))
+    INR=USD*74.24
+    print('%0.3f USD is equal to %0.3f INR' %(USD, INR))
+elif(val=='MXN'):
+    USD=float(input('Enter the amount in USD:'))
+    MXN=USD*20.02
+    print('%0.3f dollars is equal to %0.3f Mexican Pesos' %(USD, MXN))
+elif(val=='EUR'):
+    USD=float(input('Enter the amount in USD:'))
+    EUR=USD*0.85
+    print('%0.3f dollars is equal to %0.3f euros' %(USD, EUR))
+elif(val=='JPY'):
+    USD=float(input('Enter the amount in USD:'))
+    JPY=USD*110.25
+    print('%0.3f dollars is equal to %0.3f Japanese Yen' %(USD, JPY))
+elif(val=='AUD'):
+    USD=float(input('Enter the amount in USD:'))
+    AUD=USD*1.36
+    print('%0.3f dollars is equal to %0.3f Australian Dollars' %(USD, AUD))
+elif(val=='CAD'):
+    USD=float(input('Enter the amount in USD:'))
+    CAD=USD*1.25
+    print('%0.3f dollars is equal to %0.3f canadian dollars' %(USD, CAD))
+elif(val=='NPR'):
+    USD=float(input('Enter the amount in USD:'))
+    NPR=USD*118.66
+    print('%0.3f dollars is equal to %0.3f Nepalese Rupees' %(USD, NPR))
+elif(val=='GBP'):
+    USD=float(input('Enter the amount in USD:'))
+    GBP=USD*0.72
+    print('%0.3f dollars is equal to %0.3f pound sterling' %(USD, GBP))
+elif(val=='PKR'):
+    USD=float(input('Enter the amount in USD:'))
+    PKR=USD*163.10
+    print('%0.3f dollars is equal to %0.3f pakistan rupees' %(USD, PKR))
+elif(val=='NZD'):
+    USD=float(input('Enter the amount in USD:'))
+    NZD=USD*1.43
+    print('%0.3f dollars is equal to %0.3f new zealand dollar' %(USD, NZD))
+elif(val=='CNY'):
+    USD=float(input('Enter the amount in USD:'))
+    CNY=USD*6.48
+    print('%0.3f dollars is equal to %0.3f chinese yuan' %(USD, CNY))
+elif(val=='AZN'):
+    USD=float(input('Enter the amount in USD:'))
+    AZN=USD*1.69
+    print('%0.3f dollars is equal to %0.3f AZN' %(USD, AZN))
+elif(val=='ISK'):
+    USD=float(input('Enter the amount in USD:'))
+    ISK=USD*125.51
+    print('%0.3f dollars is equal to %0.3f Icelandic Krona' %(USD, ISK))
+elif(val=='RUB'):
+    USD=float(input('Enter the amount in USD:'))
+    RUB=USD*73.5
+    print('%0.3f dollars is equal to %0.3f Russian Ruble' %(USD, RUB))
+elif(val=='DOP'):
+    USD=float(input('Enter the amount in USD:'))
+    DOP=USD*57
+    print('%0.3f dollars is equal to %0.3f dominican peso' %(USD, DOP))
+elif(val=='CHF'):
+    USD=float(input('Enter the amount in USD:'))
+    CHF=USD*0.91
+    print('%0.3f dollars is equal to %0.3f swiss franc' %(USD, CHF))
+elif(val=='SGB'):
+    USD=float(input('Enter the amount in USD:'))
+    SGB=USD*1.35
+    print('%0.3f dollars is equal to %0.3f singapore dollar' %(USD, SGB))
+elif(val=='HKD'):
+    USD=float(input('Enter the amount in USD:'))
+    HKD=USD*7.78
+    print('%0.3f dollars is equal to %0.3f Honk Kong dollar' %(USD, HKD))
+elif(val=='HKD'):
+    USD=float(input('Enter the amount in USD:'))
+    HKD=USD*7.78
+    print('%0.3f dollars is equal to %0.3f Honk Kong dollar' %(USD, HKD))
+elif(val=='THB'):
+    USD=float(input('Enter the amount in USD:'))
+    THB=USD*33.45
+    print('%0.3f dollars is equal to %0.3f Thai Bhat' %(USD, THB))
